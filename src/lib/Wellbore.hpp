4f6a1e14dd12867053059652c807a95a17e87089
Igor Shovkun <igshov@gmail.com>
2017-11-09 20:46:57 -0600

    Writing code to locate wells

--- src/lib/Wellbore.hpp
+++ src/lib/Wellbore.hpp
@@ -88,12 +88,15 @@ namespace Wellbore
        We make three checks:
        1. Calculate the vector d that starts in p and is perpendicular to the
        well segment. If (p+d) is not in the cell, skip the segment.
-       2. if (p+d) is in the cell an lies within the segment, add the segment
-       3. if (p+d) is in the cell but doesn't lie within the segment,
-       check whether the end-points of the segment are in the cell and add the
-       segment if needed.
-       4. if segment is aligned with the cell face, add it only if the neighbor
-       cell doesn't contain it already.
+       2. if (p+d) is in the cell but lies out the segment
+       check whether the end-points of the segment are in the cell.
+       Skip segment if not.
+       3. If either of previous conditions is satisfied the segment is in the cell.
+       But....!
+       4. We check whether the wellbore is aligned with the cell face, and if
+       yes, assign it to only one cell:
+       if face_normal \cdot a = 0 (or small number)
+       and goes through the cell... How to check it???
      */
 
     Point<dim> x0, x1, p;
@@ -147,13 +150,24 @@ namespace Wellbore
           x0 = locations[i-1];
           x1 = locations[i];
           a = x1 - x0;
-          a = a/a.norm();
+          double x0x1_norm = a.norm();
+          a = a/x0x1_norm;
           // distance from cell center to the line
           d = (x0-p) - a*scalar_product(x0-p, a);
+          Point<dim> pd(p+d);
 
-          // if (!cell->point_inside(Point<dim>(p+d)))
-          //   continue;
-        }
+          if (!cell->point_inside(pd))
+            continue;
+
+          // check if the distance vector d is between x1 and x0
+          // (p+d) = x0 + a*td
+          double td = - scalar_product((x0 - p), a);
+          // x1 = x0 + a*t1
+          double t1 = x0x1_norm;
+          if((td < 0 || td > t1) && // distance vector outside segment
+             (!cell->point_inside(x0) || !cell->point_inside(x1))) //end-points
+            continue();
+        } // end loop segments
     }
   }  // eom
 }  // end of namespace
