334bd7937f6eb461ed41dbe19ffb33a6800d96c1
Igor Shovkun <igshov@gmail.com>
2018-02-20 18:25:23 -0600

    Added poroelastic test

--- test/test_poroelastic/test_poroelastic.cc
+++ test/test_poroelastic/test_poroelastic.cc
@@ -371,5 +371,47 @@ void Simulator<dim>::run()
 
 } // eom
 
-
 } // end of namespace
+
+
+int main(int argc, char *argv[])
+{
+  try
+  {
+    using namespace dealii;
+    dealii::deallog.depth_console (0);
+    Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+    // std::string input_file_name = Parsers::parse_command_line(argc, argv);
+    // std::string input_file_name = SOURCE_DIR "/../data/wo-3x3.data";
+    std::string input_file_name = SOURCE_DIR "/../data/sf-elastic-3x3.data";
+    Wings::Simulator<3> problem(input_file_name);
+    problem.run();
+    return 0;
+  }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+                << exc.what() << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+
+      return 1;
+    }
+  catch (...)
+    {
+      std::cerr << std::endl << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+
+  return 0;
+}
